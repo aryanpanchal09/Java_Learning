896af00499257a498b0973ba2b7a1a31f4ae5528
Aryan <Aryantech03@gmail.com>
2024-08-24 09:34:18 +0530

    --allow-empty-message

---
+++ Simple Files/Loops.java
@@ -0,0 +1,21 @@
+public class Loops 
+{
+    public static void main(String[] args) 
+    {
+        int i = 1;
+
+        while(i<=4)
+        {
+            System.out.println("hii " + i);   
+            int j = 1;
+            while(j<=3)
+            {
+                System.out.println("Hello " + j); 
+                j++;
+            }
+            i++;
+        }
+        System.out.println("Bye " + i);
+        
+    }    
+}
