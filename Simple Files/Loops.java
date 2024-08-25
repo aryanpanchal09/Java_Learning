123c4505abb452937327bd9e762461dd3735dd6f
Aryan <Aryantech03@gmail.com>
2024-08-25 09:12:57 +0530

    --allow-empty-message

--- Simple Files/Loops.java
+++ Simple Files/Loops.java
@@ -2,20 +2,26 @@ public class Loops
 {
     public static void main(String[] args) 
     {
-        int i = 1;
+        int i = 5;
 
-        while(i<=4)
-        {
-            System.out.println("hii " + i);   
-            int j = 1;
-            while(j<=3)
-            {
-                System.out.println("Hello " + j); 
-                j++;
-            }
-            i++;
-        }
-        System.out.println("Bye " + i);
+        // while(i<=4)
+        // {
+        //     System.out.println("hii " + i);   
+        //     int j = 1;
+        //     while(j<=3)
+        //     {
+        //         System.out.println("Hello " + j); 
+        //         j++;
+        //     }
+        //     i++;
+        // }
+        // System.out.println("Bye " + i);
         
+
+        do
+        {
+            System.out.println("Hii " + i);
+            i++;
+        }while(i<=7);
     }    
 }
