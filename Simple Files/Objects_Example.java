dcc3d5aa111d0ec1ccddb4ea5b3d6e69567920f1
Aryan <Aryantech03@gmail.com>
2024-08-26 09:06:00 +0530

    --allow-empty-message

--- Simple Files/Objects_Example.java
+++ Simple Files/Objects_Example.java
@@ -2,20 +2,29 @@ public class Objects_Example
 {
     public static void main(String[] args) 
     {
-        int num1 = 1; 
+        int num1 = 1; // -> Prmitive Variable
         int num2 = 5;
-        int result = num1 + num2;
-        System.out.println(result);    
+        // int result = num1 + num2;
+        // System.out.println(result);   
+        
+
+        // calc -> Refrence Varibale
+        Calculator calc = new Calculator();
+        
+        int result = calc.add(num1,num2); 
+
+        System.out.println(result);
     }
     
 }
 
 class Calculator
 {
-    
-    public int add()
+    int a;
+
+    public int add(int n1,int n2)
     {
-        System.out.println("In add");
-        return 0;
+        int r = n1 + n2;
+        return r;
     }
 }
